ffe4f6cd99923e3619fbd4ad4d6dbfa3be3dc8af
seiler-emerson <seiler.emerson@gmail.com>
2022-09-02 20:54:44 -0300

    Create Components

--- emr_angular/src/app/app-routing.module.ts
+++ emr_angular/src/app/app-routing.module.ts
@@ -1,10 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppointmentComponent } from './crud/appointment/appointment.component';
 import { CrudModule } from './crud/crud.module';
+import { DashboardComponent } from './crud/dashboard/dashboard.component';
+import { DoctorComponent } from './crud/doctor/doctor.component';
+import { NewAppointmentComponent } from './crud/new-appointment/new-appointment.component';
+import { NewDoctorComponent } from './crud/new-doctor/new-doctor.component';
+import { NewPatientComponent } from './crud/new-patient/new-patient.component';
+import { NewUserComponent } from './crud/new-user/new-user.component';
+import { PatientComponent } from './crud/patient/patient.component';
+import { UserComponent } from './crud/user/user.component';
 import { LoginComponent } from './login/login.component';
+import { SecurityService } from './security.service';
 
 const routes: Routes = [
   { path: "", component: LoginComponent},
+  { path: "dashboard", component: DashboardComponent,canActivate: [SecurityService]},
+  { path: "appointments", component: AppointmentComponent,canActivate: [SecurityService]},
+  { path: "new-appointment", component: NewAppointmentComponent,canActivate: [SecurityService]},
+  { path: "doctor", component: DoctorComponent,canActivate: [SecurityService]},
+  { path: "new-doctor", component: NewDoctorComponent,canActivate: [SecurityService]},
+  { path: "patient", component: PatientComponent,canActivate: [SecurityService]},
+  { path: "new-patient", component: NewPatientComponent,canActivate: [SecurityService]},
+  { path: "user", component: UserComponent,canActivate: [SecurityService]},
+  { path: "new-user", component: NewUserComponent,canActivate: [SecurityService]},
 
 ];
 
--- emr_angular/src/app/crud/crud.module.ts
+++ emr_angular/src/app/crud/crud.module.ts
@@ -1,10 +1,29 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { AppointmentComponent } from './appointment/appointment.component';
+import { NewAppointmentComponent } from './new-appointment/new-appointment.component';
+import { DoctorComponent } from './doctor/doctor.component';
+import { NewDoctorComponent } from './new-doctor/new-doctor.component';
+import { PatientComponent } from './patient/patient.component';
+import { NewPatientComponent } from './new-patient/new-patient.component';
+import { NewUserComponent } from './new-user/new-user.component';
+import { UserComponent } from './user/user.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    DashboardComponent,
+    AppointmentComponent,
+    NewAppointmentComponent,
+    DoctorComponent,
+    NewDoctorComponent,
+    PatientComponent,
+    NewPatientComponent,
+    NewUserComponent,
+    UserComponent
+  ],
   imports: [
     CommonModule
   ]
